1703d67de23d471b3d3bc14b20a63386f554fea0
Francois Brill <f@brill.co.za>
2022-10-26 20:57:00 -0700

    Add some on-page animation

--- pages/photo/[photoId].js
+++ pages/photo/[photoId].js
@@ -1,9 +1,3 @@
-import Link from "next/link";
-import Layout from "../../components/Layout";
-import Image from "next/image";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import { photos } from "../../data/photos";
 import {
   ArrowSmallLeftIcon,
   ArrowUpRightIcon,
@@ -11,6 +5,13 @@ import {
   CameraIcon,
   MapPinIcon,
 } from "@heroicons/react/24/outline";
+import Link from "next/link";
+import Layout from "../../components/Layout";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { photos } from "../../data/photos";
+import { motion } from "framer-motion";
 import { LoremIpsum } from "react-lorem-ipsum";
 
 const PhotoDetail = () => {
@@ -31,7 +32,14 @@ const PhotoDetail = () => {
       <div className="grid grid-cols-[1fr,1.5fr] min-h-screen">
         <div className="p-10">
           <div className="sticky top-10">
-            <div className="flex justify-between mb-8">
+            <motion.div
+              className="flex justify-between mb-8"
+              initial={{ x: 100, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{
+                delay: 0.2,
+              }}
+            >
               <Link href="/">
                 <a className="inline-flex items-center rounded-md border border-gray-300 bg-white pl-3 pr-4 py-2 text-sm font-medium text-gray-700 hover:text-white hover:bg-purple-600 hover:border-purple-600 focus:border-purple-200 focus:outline-0 focus:ring-2 focus:ring-purple-300 group">
                   <ArrowSmallLeftIcon className="w-5 h-5 mr-2 text-gray-300 group-hover:text-purple-200" />
@@ -47,9 +55,25 @@ const PhotoDetail = () => {
                 View Source
                 <ArrowUpRightIcon className="w-4 h-4 text-gray-400 ml-2 group-hover:text-purple-400" />
               </a>
-            </div>
-            <h2 className="text-4xl font-bold tracking-tight">{photo.title}</h2>
-            <div className="mt-5 text-sm text-gray-600 space-y-2">
+            </motion.div>
+            <motion.h2
+              initial={{ x: 100, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{
+                delay: 0.4,
+              }}
+              className="text-4xl font-bold tracking-tight"
+            >
+              {photo.title}
+            </motion.h2>
+            <motion.div
+              initial={{ x: 100, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{
+                delay: 0.6,
+              }}
+              className="mt-5 text-sm text-gray-600 space-y-2"
+            >
               <p className="flex items-center">
                 <CameraIcon className="w-4 h-4 mr-2 text-purple-500" />
                 {photo.author}
@@ -62,10 +86,17 @@ const PhotoDetail = () => {
                 <CalendarDaysIcon className="w-4 h-4 mr-2 text-purple-500" />
                 {photo.date}
               </p>
-            </div>
-            <p className="mt-5 text-sm text-gray-500 space-y-5">
-              <LoremIpsum p={4} />
-            </p>
+            </motion.div>
+            <motion.div
+              initial={{ x: 100, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{
+                delay: 0.8,
+              }}
+              className="mt-5 text-sm text-gray-500 space-y-5"
+            >
+              <LoremIpsum p={3} />
+            </motion.div>
           </div>
         </div>
         <div className="overflow-hidden flex">
