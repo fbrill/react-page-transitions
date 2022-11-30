5157ace408ed597b2bd1311c316f8e0c1f91f6a1
Francois Brill <f@brill.co.za>
2022-11-30 08:44:48 -0800

    fix: deprecated exitBeforeEnter

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import { AnimatePresence } from "framer-motion";
 function MyApp({ Component, pageProps, router }) {
   return (
     <AnimatePresence
-      exitBeforeEnter
+      mode="wait"
       initial={false}
       onExitComplete={() => window.scrollTo(0, 0)}
     >
